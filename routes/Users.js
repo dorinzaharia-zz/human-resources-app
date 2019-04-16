062bd4d217fd07426dedbbd6fcf387bd85221840
dorinzaharia <dorin.zahariazd@gmail.com>
2019-04-16 20:17:05 +0300

    Add get, delete, and patch users

--- routes/Users.js
+++ routes/Users.js
@@ -7,67 +7,171 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 users.use(cors());
 
+const checkToken = (req, res, next) => {
+    const header = req.headers["authorization"];
+
+    if (typeof header !== "undefined") {
+        const bearer = header.split(" ");
+        const token = bearer[1];
+        req.token = token;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
 // User registration
 users.post("/register", (req, res) => {
-  const currentDate = new Date();
-  const userData = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    password: req.body.password,
-    created: currentDate
-  };
+    const currentDate = new Date();
+    const userData = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        created: currentDate
+    };
 
-  // Check if user exists
-  User.findOne({
-    email: req.body.email
-  })
-    .then(user => {
-      if (!user) {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          userData.password = hash;
-          User.create(userData)
-            .then(user => {
-              res.status(201).json({ message: user.email + " registered!" });
-            })
-            .catch(err => {
-              res.status(500).json({ error: err });
-            });
-        });
-      } else {
-        res.status(302).json({ error: "User already exists" });
-      }
+    // Check if user exists
+    User.findOne({
+        email: req.body.email
     })
-    .catch(err => {
-      res.status(500).json({ error: err });
-    });
+        .then(user => {
+            if (!user) {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    userData.password = hash;
+                    User.create(userData)
+                        .then(user => {
+                            res.status(201).json({
+                                message: user.email + " registered!"
+                            });
+                        })
+                        .catch(err => {
+                            res.status(500).json({ error: err });
+                        });
+                });
+            } else {
+                res.status(302).json({ error: "User already exists" });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
 });
 
 // User login
 users.post("/login", (req, res) => {
-  User.findOne({
-    email: req.body.email
-  })
-    .then(user => {
-      if (user) {
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-          const payload = {
-            _id: user._id,
-            email: user.email
-          };
-          let token = jwt.sign(payload, process.env.SECRET_KEY, {
-            expiresIn: 2440
-          });
-          res.status(200).json({ "JWT token": token });
-        } else {
-          res.status(403).json({ error: "Incorrect password." });
-        }
-      } else {
-        res.status(404).json({ error: "User does not exist" });
-      }
+    User.findOne({
+        email: req.body.email
     })
-    .catch(err => {
-      res.status(500).json({ error: err });
+        .then(user => {
+            if (user) {
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    const payload = {
+                        _id: user._id,
+                        email: user.email
+                    };
+                    let token = jwt.sign(payload, process.env.SECRET_KEY, {
+                        expiresIn: 2440
+                    });
+                    res.status(200).json({ Token: token });
+                } else {
+                    res.status(403).json({ error: "Incorrect password." });
+                }
+            } else {
+                res.status(404).json({ error: "User does not exist" });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+});
+
+// Get users data
+users.get("/", checkToken, (req, res) => {
+    //verify the JWT token generated for the user
+    jwt.verify(req.token, process.env.SECRET_KEY, err => {
+        if (err) {
+            res.status(403).json({
+                error: "Could not connect to the protected route"
+            });
+        } else {
+            User.find()
+                .then(docs => {
+                    const response = {
+                        users: docs.map(doc => doc)
+                    };
+                    res.status(200).json(response);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        error: err
+                    });
+                });
+        }
+    });
+});
+
+// Update users data
+users.patch("/:id", checkToken, (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, err => {
+        if (err) {
+            res.status(403).json({
+                error: "Could not connect to the protected route"
+            });
+        } else {
+            const id = req.params.id;
+
+            if (req.body.password) {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    req.body.password = hash;
+                    User.updateOne({ _id: id }, { $set: req.body })
+                        .then(result => {
+                            res.status(200).json(result);
+                        })
+                        .catch(err => {
+                            res.status(500).json({ error: err });
+                        });
+                });
+            } else {
+                User.updateOne({ _id: id }, { $set: req.body })
+                    .then(result => {
+                        res.status(200).json(result);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
+            }
+        }
+    });
+});
+
+// Delete users data
+users.delete("/:id", checkToken, (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, err => {
+        if (err) {
+            res.status(403).json({
+                error: "Could not connect to the protected route"
+            });
+        } else {
+            const id = req.params.id;
+            User.remove({ _id: id })
+                .then(result => {
+                    res.status(200).json({
+                        message: "User deleted"
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        error: err
+                    });
+                });
+        }
     });
 });
 
--- server.js
+++ server.js
@@ -11,19 +11,19 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
+    bodyParser.urlencoded({
+        extended: false
+    })
 );
 
 // Connection to MongoDB
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${
-  process.env.DB_PASSWORD
+    process.env.DB_PASSWORD
 }${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}`;
 mongoose
-  .connect(uri, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+    .connect(uri, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
 
 var Users = require("./routes/Users");
 
@@ -31,5 +31,5 @@ app.use("/users", Users);
 
 // Run server
 app.listen(port, () => {
-  console.log("Server is running on port: " + port);
+    console.log("Server is running on port: " + port);
 });
